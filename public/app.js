06f49af7f00160c97b61cae2d70bc1cddeb4941f
Anfal Boussayoud <anfal.boussayoud@gmail.com>
2015-05-05 20:47:22 -0400

    updated flashcards

--- public/app.js
+++ public/app.js
@@ -1,30 +1,38 @@
 var app = angular.module('FlashCards', []);
 
-// app.controller('FlashCardController', function($scope) {
-	
-// 	$scope.answerQuestion = function (answer) {
-// 		console.log(answer);
-// 	};
+ app.controller('FlashCardController', function($scope) {
 
-// 	$scope.flashCard = {
-// 		question: 'What is Angular?',
-// 		answers: [
-// 		{ text: 'A front-end framework for great power!', correct: true },
-// 		{ text: 'Something lame, who cares, whatever.', correct: false },
-// 		{ text: 'Some kind of fish, right?', correct: false }
-// 		]
-// 	};
+     $scope.answerQuestion = function (answer) {
+         console.log(answer);
+
+         if($scope.answered) {
+             return;
+         }
+
+         $scope.answered = true;
+
+         $scope.answeredCorrectly = answer;
+
+     };
 
 
 
-// });
+
+ });
 
 
 app.controller('MainController', function($scope) {
 
-	$scope.answerQuestion = function (answer) {
-		console.log(answer);
-	};
+	//$scope.answerQuestion = function (answer) {
+	//	console.log(answer);
+    //
+	//	if (answer == "true") {
+	//		$scope.answered = "answeredCorrectly";
+	//	} else {
+	//		$scope.answered = "!answeredCorrectly";
+	//	}
+	//};
+
 	
 
 	$scope.flashCards = [
